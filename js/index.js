d7f84d80b4a27c681ac608510e7a304bcd93753c
Mario Pierre-Louis Marín <b.mplm@tid.es>
2014-02-03 21:55:30 +0100

    índice escrito

--- js/index.js
+++ js/index.js
@@ -1,12 +1,10 @@
 
-var socket = io.connect('http://192.168.1.103:3000');
-
-socket.on('connect', function () {
-});
-
 document.querySelector('#endpoint').addEventListener ('click', function () {
 
 	alert('Tu endpoint es: ');
 
 });
+
+$("a.youtube").YouTubePopup({ autoplay: 1, draggable: true });
+
 
--- server.js
+++ server.js
@@ -1,18 +1,12 @@
 var fs = require('fs'),
 path = require('path'),
-sio = require('socket.io'),
 static = require('node-static'),
 sanitize = require('validator').sanitize,
 
-MongoClient = require('mongodb').MongoClient
+	// (err) throw err;
 
-MongoClient.connect('mongodb://127.0.0.1:27017/cf', function(err, db) {
-	if (err) throw err;
-
-	var app = require('http').createServer(handler);
-	var visitas = 1;
-	var day = 0;
-	publicIps = [];
+	app = require('http').createServer(handler);
+	
 	app.listen(3000);
 
 	var file = new static.Server(path.join(__dirname,'/'));
@@ -21,46 +15,3 @@ MongoClient.connect('mongodb://127.0.0.1:27017/cf', function(err, db) {
 		file.serve(req, res);
 	}
 
-	var io = sio.listen(app);
-
-io.set('log level', 1);
-
-	io.sockets.on('connection', function (socket) {
-
-		visitas++
-
-		socket.on('ipinfo', function(info){
-			console.log('ip recibida');
-
-			var today = new Date();
-			var dd = today.getDate();
-			var mm = today.getMonth()+1; 
-			var yyyy = today.getFullYear();
-			var fecha = dd + '/' + mm + '/' + yyyy;
-
-			if(dd != day){
-				publicIps = [info];
-				visitas = 1;
-				day = dd;
-				db.collection('visitas', function(err,collection){
-                    doc = {"fecha": fecha, "unicas": publicIps.length, "impresiones": visitas};
-                    collection.insert(doc, function(){});
-                });
-
-			} else {
-					var index = publicIps.indexOf(info);
-					if(index == -1){
-						publicIps.push(info)				                    
-			    	}
-			    	updateIps(fecha);
-			 }
-		});
-
-	function updateIps(fecha){		
-		db.collection('visitas').update({fecha: fecha}, {$set: {unicas: publicIps.length, impresiones:visitas}}, {w:1}, function(err) {
-                                if (err) console.warn(err.message);
-                        });
-	}
-
-	});
-});
