e2f34aa738949ef5e048c722caf6814c101625ce
root <root@vps41542.ovh.net>
2014-02-16 01:01:35 +0100

    node.js deleted

---
+++ cgo/js/index.js
@@ -0,0 +1,9 @@
+
+
+// $("a.youtube").YouTubePopup({ autoplay: 1, draggable: true });
+
+$(function () {
+    $("a.youtube").YouTubePopup(options);
+});
+
+$("#youtubeLink").YouTubePopup({ youtubeId: '4eYSpIz2FjU', title: 'My New Title' });
